673369541daadd4420f575b8ec491eb1f63a4ac1
Shians <registertonysu@gmail.com>
2022-11-08 13:24:23 +1100

    Changed race_id to use logcounts

--- R/race_id.R
+++ R/race_id.R
@@ -4,13 +4,12 @@
 #' @return SingleCellExperiment object
 #' @export
 cluster_race_id <- function(sce) {
-    counts <- SingleCellExperiment::counts(sce)
-
-    sc <- RaceID::SCseq(counts)
+    sc <- RaceID::SCseq(logcounts(sce))
     sc <- RaceID::filterdata(sc, mintotal = 1)
     sc <- RaceID::compdist(sc)
     sc <- RaceID::clustexp(sc)
     sc <- RaceID::findoutliers(sc)
+
     res <- factor(sc@cpart)
 
     colData(sce)$cluster_id <- res
