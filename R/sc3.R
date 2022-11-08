3153e0f57c161a67753f4144d7f626b990dd5e95
Shians <registertonysu@gmail.com>
2022-11-08 15:34:40 +1100

    SC3 removed due to unclear documentation referencing deprecated scater functionality

--- R/sc3.R
+++ R/sc3.R
@@ -4,8 +4,6 @@ pre_clean <- function(sce) {
     ave.counts <- rowMeans(counts(sce))
     keep <- ave.counts >= 1
     sce_cleaned <- sce[keep,]
-    sce_cleaned <- scater::perCellQCMetrics(sce_cleaned)
-    sce_cleaned <- scater::normalize(sce_cleaned)
     return(sce_cleaned)
 }
 
