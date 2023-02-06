49f9f6ff51787810e423755f151fca14e529c7f2
Shians <registertonysu@gmail.com>
2023-02-06 13:55:49 +1100

    Removed SC3

--- R/all_cluster_methods.R
+++ R/all_cluster_methods.R
@@ -2,7 +2,6 @@
 all_cluster_methods <- function() {
     list(
         race_id = cluster_race_id,
-        sc3 = cluster_sc3,
         seurat = cluster_seurat,
         tscan = cluster_tscan
     )
